6b28fb416e9a44db79475fcd947f084fd899023d
iGo96 <vukajlovic,igor@hotmail.it>
2016-03-16 11:38:01 +0100

    x

--- Esercizio 2.js
+++ Esercizio 2.js
@@ -1,8 +1,8 @@
 function sumIntDisp(n){
 	//Dato un numero n, restituire la somma dei primi n numeri interi positivi dispari 
 	//itterativo
+	var tot = 0;
 	for (i=0;i<n;i++){
-		if(i%2)  //scopro se il numero è dispari
 	}
 }
 
